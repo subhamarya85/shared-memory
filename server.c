f167b42a86ecb5282182a7d31fc0d028a9e24633
subhamarya85 <subhamarya85@gmail.com>
2021-04-04 16:56:22 +0530

    hello world

---
+++ server.c
@@ -0,0 +1,55 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+#include<sys/types.h>
+#include<sys/ipc.h>
+#include<sys/shm.h>
+
+
+#define SHSIZE 100
+
+
+
+int main(int argc, char argv[])
+{
+
+	int shmid;
+	key_t key;
+	char *shm;
+	char *s;
+
+
+	key= 9976;
+
+	shmid = shmget(key,SHSIZE, IPC_CREAT | 0666);
+
+	if(shmid<0)
+	{
+		perror("shmget");
+		exit(1);
+
+	}
+
+	shm = shmat(shmid, NULL , 0);
+
+	if(shm == (char *)-1)
+	{
+		perror("shmat");
+		exit(1);
+	}
+
+	memcpy(shm, "HELLO WORLD",11);
+
+	s = shm;
+
+	s+=11;
+
+	*s = 0;
+
+	while(*shm != '*')
+		sleep(1);
+
+	return 0;
+
+}
